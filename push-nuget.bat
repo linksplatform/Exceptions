ae64aeac34a45535f4a3cc92c6b83f6ee00e4774
Konstantin Dyachenko <konard@me.com>
2019-07-14 19:49:33 +0300

    Update push-nuget.bat

--- push-nuget.bat
+++ push-nuget.bat
@@ -1,8 +1,8 @@
 dotnet pack -c Release --include-symbols
 cd bin\Release
-dotnet nuget push -s https://api.nuget.org/v3/index.json *.symbols.nupkg -k oy2jadomp2dedanxgvgafhmeykopm3rebdcvnec3ykedm4
+dotnet nuget push -s https://api.nuget.org/v3/index.json *.symbols.nupkg
 del *.symbols.nupkg
-dotnet nuget push -s https://api.nuget.org/v3/index.json *.nupkg -k oy2jadomp2dedanxgvgafhmeykopm3rebdcvnec3ykedm4
+dotnet nuget push -s https://api.nuget.org/v3/index.json *.nupkg
 del *.nupkg
 cd ..
 cd ..
