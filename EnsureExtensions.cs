96b9f4a61a074ee651f4202274ec5bafca7c4c93
Константин Дьяченко <konard@me.com>
2019-08-03 10:53:30 +0300

    Closes #11.

--- EnsureExtensions.cs
+++ EnsureExtensions.cs
@@ -12,21 +12,34 @@ namespace Platform.Exceptions
         #region Always
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ArgumentNotNull<TArgument>(this EnsureAlwaysExtensionRoot root, TArgument argument, string argumentName)
-           where TArgument : class
+        public static void ArgumentNotNull<TArgument>(this EnsureAlwaysExtensionRoot root, TArgument argument, string argumentName, string message)
+            where TArgument : class
         {
             if (argument == null)
             {
-                throw new ArgumentNullException(argumentName);
+                throw new ArgumentNullException(argumentName, message);
             }
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ArgumentNotNull<TArgument>(this EnsureAlwaysExtensionRoot root, TArgument argument, string argumentName) where TArgument : class => root.ArgumentNotNull(argument, argumentName, $"Argument {argumentName} is null.");
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ArgumentNotNull<TArgument>(this EnsureAlwaysExtensionRoot root, TArgument argument) where TArgument : class => root.ArgumentNotNull(argument, null);
 
         #endregion
 
         #region OnDebug
 
+
+        [Conditional("DEBUG")]
+        public static void ArgumentNotNull<TArgument>(this EnsureOnDebugExtensionRoot root, TArgument argument, string argumentName, string message) where TArgument : class => Ensure.Always.ArgumentNotNull(argument, argumentName, message);
+
         [Conditional("DEBUG")]
         public static void ArgumentNotNull<TArgument>(this EnsureOnDebugExtensionRoot root, TArgument argument, string argumentName) where TArgument : class => Ensure.Always.ArgumentNotNull(argument, argumentName);
+
+        [Conditional("DEBUG")]
+        public static void ArgumentNotNull<TArgument>(this EnsureOnDebugExtensionRoot root, TArgument argument) where TArgument : class => Ensure.Always.ArgumentNotNull(argument);
 
         #endregion
     }
