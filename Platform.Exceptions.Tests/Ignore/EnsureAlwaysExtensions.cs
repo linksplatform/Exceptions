c9141205e7eb99aa2c14de2326d938c34e4809e5
Константин Дьяченко <konard@me.com>
2019-10-21 19:54:27 +0300

    Codacy issue fix.

--- Platform.Exceptions.Tests/Ignore/EnsureAlwaysExtensions.cs
+++ Platform.Exceptions.Tests/Ignore/EnsureAlwaysExtensions.cs
@@ -9,6 +9,7 @@ namespace Platform.Exceptions.Tests.Ignore
         public static void ArgumentNotNull<TArgument>(this EnsureAlwaysExtensionRoot root, TArgument argument, string argumentName)
             where TArgument : class
         {
+            // Override logic to do nothing (this should be used to reduce the overhead of the Ensure checks, when it is critical to performance)
         }
     }
 }
