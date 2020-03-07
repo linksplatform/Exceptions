cec390f1da45d3a8c7625bd5184f49f16a439863
Konstantin Diachenko <drakonard@gmail.com>
2020-03-07 06:58:25 +0300

    Using new version of translator for tests.

--- cpp/Platform.Exceptions.Tests/AllTests.cpp
+++ cpp/Platform.Exceptions.Tests/AllTests.cpp
@@ -3,5 +3,5 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 #include "EnsuranceTests.cpp"
 // Due to the nature of extension methods these tests do not apply to C++
-//#include "EnsureExtensions.cpp"
-//#include "IgnoredEnsuranceTests.cpp"
+//#include "Ignore/EnsureExtensions.cpp"
+//#include "Ignore/IgnoredEnsuranceTests.cpp"
---
+++ cpp/Platform.Exceptions.Tests/Ignore/EnsureExtensions.cpp
@@ -0,0 +1,9 @@
+﻿namespace Platform::Exceptions::Tests::Ignore
+{
+    class EnsureExtensions
+    {
+        public: template <typename TArgument> static void ArgumentNotNull(Platform::Exceptions::ExtensionRoots::EnsureAlwaysExtensionRoot root, TArgument* argument, const char* argumentName)
+        {
+        }
+    };
+}
