726ac1b61f4af555bab14331d0b99c12ad475c9a
konard <drakonard@gmail.com>
2020-12-18 17:20:10 +0300

    AllTests.cpp fix.

--- cpp/Platform.Exceptions.Tests/AllTests.cpp
+++ cpp/Platform.Exceptions.Tests/AllTests.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-#include "EnsuranceTests.cpp"
+#include "EnsuranceTests.h"
 // Due to the nature of extension methods these tests do not apply to C++
 //#include "Ignore/EnsureExtensions.cpp"
 //#include "Ignore/IgnoredEnsuranceTests.cpp"
