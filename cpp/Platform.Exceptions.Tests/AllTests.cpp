f16a8224509ac5a4abd5073551469e4b0c7532f5
konard <drakonard@gmail.com>
2020-12-18 17:30:24 +0300

    Tests translation fix.

--- cpp/Platform.Exceptions.Tests/AllTests.cpp
+++ cpp/Platform.Exceptions.Tests/AllTests.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-#include "EnsuranceTests.h"
+#include "EnsuranceTests.cpp"
 // Due to the nature of extension methods these tests do not apply to C++
-//#include "Ignore/EnsureExtensions.h"
-//#include "Ignore/IgnoredEnsuranceTests.h"
+//#include "Ignore/EnsureExtensions.cpp"
+//#include "Ignore/IgnoredEnsuranceTests.cpp"
