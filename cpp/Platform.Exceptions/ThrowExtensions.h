ace1555b06ab63a7aef4f0e2cc69b2485d378888
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-09 10:57:28 +0600

    Add exceptions

--- cpp/Platform.Exceptions/ThrowExtensions.h
+++ cpp/Platform.Exceptions/ThrowExtensions.h
@@ -7,4 +7,12 @@
     void NotImplementedException() { throw std::logic_error("Not implemented exception."); }
 
     auto NotImplementedExceptionAndReturn() { throw std::logic_error("Not implemented exception."); }
+
+    void ArgumentLinkDoesNotExistsException() { throw std::logic_error("The passed link does not exists."); }
+
+    void ArgumentLinkHasDependenciesException() { throw std::logic_error("The passed link has dependencies that prevents changes of the passed link inner structure."); }
+
+    void LinkWithSameValueAlreadyExistsException() { throw std::logic_error("Link with same value already exists."); }
+
+    void LinksLimitReachedException() { throw std::logic_error("Storage links limit has been reached."); }
 }
